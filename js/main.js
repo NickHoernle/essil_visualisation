3252df71de9b4f8261c0be979f68f0d4c35ad0a3
Nick Hoernle <nicholas.hoernle@gmail.com>
2018-10-17 20:32:09 +0100

    initial commit for sharing this repo

---
+++ js/main.js
@@ -0,0 +1,223 @@
+var BIOMES = ['Desert_Water', 'Plains_Water', 'Jungle_Water', 'Wetlands_Water']
+var PLANTS = ['Desert_Plants', 'Plains_Plants', 'Jungle_Plants', 'Wetlands_Plants']
+var LEVELS = ['lv1', 'lv2', 'lv3', 'lv4']
+var COLORS = ['green', 'blue', 'red', 'purple'];
+
+
+/*
+Sort out the video stuffs here
+ */
+var vid = document.getElementById("globalView");
+var speed = 3.0;
+vid.playbackRate = speed;
+
+
+/*
+D3v4 magic goes in here
+*/
+
+
+
+var margin = {top: 30, right: 20, bottom: 30, left: 50};
+
+//====================================================================
+// USEFUL FUNCTIONS
+
+// Get the data
+var waterLevelsAndPeriods = null;
+var fix_seconds = [];
+var seen = [];
+var zoomedChart = null;
+var mainChart = null;
+var plants = null;
+
+d3.csv("data/test5_res.csv", function(error, data) {
+
+    var mapped_data = data.map(function(d) {
+        var dte = new Date(2014, 4, 1);
+        var u = +dte;
+        var seconds = parseInt(d.seconds);
+        var newU = u + seconds*1000;
+        var newD = new Date(newU);
+
+        var datapoint = {};
+        datapoint.seconds = d.seconds;
+        datapoint.start = d.start;
+        datapoint.stop = d.stop;
+        datapoint.date = newD;
+        datapoint.Desert_Water = +parseFloat(d.Desert_Water);
+        datapoint.Jungle_Water = +parseFloat(d.Jungle_Water);
+        datapoint.Wetlands_Water = +parseFloat(d.Wetlands_Water);
+        datapoint.Plains_Water = +parseFloat(d.Plains_Water);
+        datapoint.Desert_Plants = +parseFloat(d.Desert_Plants);
+        datapoint.Jungle_Plants = +parseFloat(d.Jungle_Plants);
+        datapoint.Wetlands_Plants = +parseFloat(d.Wetlands_Plants);
+        datapoint.Plains_Plants = +parseFloat(d.Plains_Plants);
+        datapoint.vid_sec = +parseInt(d.vid_sec);
+        datapoint.important = +parseInt(d.interesting);
+
+        PLANTS.forEach(function(plant) {
+            LEVELS.forEach(function(level) {
+                var value = plant.replace('Plants', level);
+                datapoint[value] = +parseInt(d[value]);
+            });
+        });
+
+
+        var top = ['top1', 'top2', 'top3', 'top4'];
+        var bottom = ['bottom1', 'bottom2', 'bottom3', 'bottom4'];
+
+        datapoint.top = top.map( function(x) { return (x=='None' ? null : parseInt(d[x]) + 1) }).filter( (v) => v ).map( function(x) { return x-1; });
+        datapoint.bottom = bottom.map( function(x) { return (x=='None' ? null : parseInt(d[x]) + 1) }).filter( (v) => v ).map( function(x) { return x-1; });
+
+        if ((seen.indexOf(d.vid_sec) == -1)) {
+            seen.push(d.vid_sec)
+            fix_seconds.push(seconds)
+        }
+
+        return datapoint;
+    });
+
+    waterLevelsAndPeriods = mapped_data;
+
+    zoomedChart = new LineChart(
+        data = waterLevelsAndPeriods,
+        width = $('#zoomed_chart').width() - margin.left - margin.right,
+        margin = margin,
+        height = $('#zoomed_chart').height() - margin.top - margin.bottom,
+        colors = COLORS,
+        element = '#zoomed_chart'
+    );
+    zoomedChart.plotLegend(BIOMES);
+
+    mainChart = new LineChart(
+        data = waterLevelsAndPeriods,
+        width = $('#full_chart').width() - margin.left - margin.right,
+        margin = margin,
+        height = $('#full_chart').height() - margin.top - margin.bottom,
+        colors = COLORS,
+        element = '#full_chart'
+    );
+    mainChart.plotBiomes(BIOMES);
+    mainChart.plotLegend(BIOMES);
+
+    plantsChart = new LineChart(
+        data = waterLevelsAndPeriods,
+        width = $('#plants_chart').width() - margin.left - margin.right,
+        margin = margin,
+        height = $('#plants_chart').height() - margin.top - margin.bottom,
+        colors = COLORS,
+        element = '#plants_chart'
+    );
+    plantsChart.plotLegend( getActivePlants() );
+});
+
+vid.ontimeupdate = function() {
+    var time = fix_seconds[parseInt(vid.currentTime)];
+
+    if (time <= 1) {
+        waterLevelsAndPeriods.forEach(function(x) {
+            var time = x.seconds;
+            mainChart.plotActiveRegions(time, false);
+        });
+    }
+    // var stop = start + 70;
+
+    zoomedChart.plotBiomes(BIOMES, time-30, time+70);
+    plantsChart.plotBiomes( getActivePlants() , time-30, time+70);
+
+    mainChart.plotTimeTracker(time+1);
+    zoomedChart.plotTimeTracker(time+1);
+    plantsChart.plotTimeTracker(time+1);
+
+    zoomedChart.plotActiveRegions(time, true);
+    plantsChart.plotActiveRegions(time, true);
+
+    mainChart.plotFocus(time);
+
+    plantsChart.plotLegend( getActivePlants() );
+
+    $('#top_biomes').empty();
+    waterLevelsAndPeriods[time].top.forEach( function(x) {
+        $('#top_biomes').append($('<li>'+BIOMES[x].replace('_Water', ' ')+'</li>').css('color', COLORS[x]));
+    });
+    $('#bottom_biomes').empty();
+    waterLevelsAndPeriods[time].bottom.forEach( function(x) {
+        $('#bottom_biomes').append($('<li>'+BIOMES[x].replace('_Water', ' ')+'</li>').css('color', COLORS[x]));
+    });
+
+};
+
+/* Plotting controls*/
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getActivePlants() {
+    var plant_checkboxes = $('.plant_checkbox');
+    return (plant_checkboxes.map(function(i, box) {
+        var checkbox_id = box.id;
+        if (box.checked) {
+            checkbox_id = checkbox_id.replace('checkbox_', '');
+            checkbox_id = checkbox_id.replace('_plant_', '_lv');
+            return capitalizeFirstLetter(checkbox_id);
+        }
+    })).toArray();
+};
+
+$('.plant_checkbox').each(function(i, box) {
+    $(box).change(function() {
+        var time = fix_seconds[parseInt(vid.currentTime)];
+        plantsChart.plotBiomes( getActivePlants() , time-30, time+70);
+        plantsChart.plotLegend( getActivePlants() );
+    });
+});
+
+$('.plant_checkbox_all').each(function(i, box) {
+    $(box).change(function() {
+        var id = box.id;
+        var biome = id.replace('checkbox_all_', '');
+        var other_boxes = [1,2,3,4].map(function(x) { return '#checkbox_' + biome + '_plant_' + x;});
+
+        other_boxes.forEach( function (value) {
+            $(value)[0].checked = box.checked;
+        });
+        var time = fix_seconds[parseInt(vid.currentTime)];
+        plantsChart.plotBiomes( getActivePlants() , time-30, time+70);
+        plantsChart.plotLegend( getActivePlants() );
+    });
+});
+
+/* video controls */
+$(document).keydown(function(e) {
+
+    switch(e.which) {
+        case 37: // left
+            var vid = $('#globalView')[0];
+            var time = vid.currentTime
+            vid.currentTime = time - 2;
+            // vid.currentTime = (start - 2);
+            break;
+
+        case 38: // up
+            break;
+
+        case 39: // right
+            var vid = $('#globalView')[0];
+            var time = vid.currentTime
+            vid.currentTime = time + 2;
+            // vid.currentTime = (start + 2);
+            break;
+
+        case 40: // down
+            break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+$('#globalView').click(function() {
+    this.paused ? this.play() : this.pause();
+});
---
+++ js/renderLineChart.js
@@ -0,0 +1,278 @@
+/**
+ * Created by nickhoernle on 2018/10/09.
+ */
+
+var format = d3.timeFormat("%M:%S")
+
+function plot_line(biome, x, y) {
+    return d3.line()
+        .x(function (d) {
+            return x(d.date);
+        })
+        .y(function (d) {
+            return y(d[biome]);
+        });
+};
+
+class LineChart  {
+    constructor(data, width, margin, height, colors, element) {
+
+        this.data = data;
+        this.width = width;
+        this.margin = margin;
+        this.height = height;
+        this.colors = function(x) {
+            var text = String(x);
+            if (text.match(/^desert.*/i) != null) { return colors[0]; }
+            else if (text.match(/^plains.*/i) != null) { return colors[1]; }
+            else if (text.match(/^jungle.*/i) != null) { return colors[2]; }
+            else if (text.match(/^wetlands.*/i) != null) { return colors[3]; }
+            else {return 'Black';}
+        };
+        var element = element;
+
+        this.x = d3.scaleTime().range([0, this.width]);
+        this.y = d3.scaleLinear().range([this.height, 0]);
+
+        this.svg = d3.select(element).append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+        this.ymax = d3.max(data, function(d) { return d3.max([d.Wetlands_Water, d.Jungle_Water, d.Plains_Water, d.Desert_Water]); });
+        this.ymin = 0;
+
+        this.x.domain(d3.extent(data, function(d) { return d.date; }));
+        this.y.domain([this.ymin, this.ymax]);
+
+        //add the X Axis
+        this.svg.append("g")
+            .attr("transform", "translate(0," + this.height + ")")
+            .attr("class", "axis")
+            .call(d3.axisBottom(this.x)
+                .ticks(10)
+                .tickFormat(format));
+
+        // add the Y Axis
+        this.svg.append("g")
+            .call(d3.axisLeft(this.y)
+                .ticks(5));
+    }
+
+    lineStyle (biome) {
+        var text = String(biome);
+        if (text.match(/.*lv1.*/i) != null) { return 'lv1'; }
+        else if (text.match(/.*lv2.*/i) != null) { return 'lv2'; }
+        else if (text.match(/.*lv3.*/i) != null) { return 'lv3'; }
+        else 'solid';
+    }
+
+    plotBiomeWater (data, biome, i) {
+        var val_line = plot_line(biome, this.x, this.y);
+
+        this.svg.append("path")
+            .attr("class", "line")
+            .attr("class", this.lineStyle(biome))
+            .style('stroke', this.colors(biome))
+            .attr("d", val_line(data));
+    }
+
+    plotBiomes (BIOMES, start=null, stop=null) {
+
+        var data = null;
+        if (start != null) {
+            var data = this.data.filter(function (d) {
+                if ((start - 30 >= 0) && (stop < this.data.slice(-1)[0].seconds)) {
+                    return (d.seconds > start - 30 && d.seconds <= stop)
+                } else if (start - 30 <= 0) {
+                    return (d.seconds > 0 && d.seconds <= 100);
+                } else {
+                    return (d.seconds > this.data.slice(-1)[0].seconds - 100);
+                }
+            }, this);
+
+            this.ymax = d3.max(data, function(d) { return 1.1*d3.max(BIOMES.map(function(biome) { return d[biome]; })) } );
+            this.ymin = 0;
+
+            this.x.domain(d3.extent(data, function(d) { return d.date; }));
+            this.y.domain([this.ymin, this.ymax]);
+
+            this.svg.selectAll("g").remove();
+            this.svg.selectAll("path").filter(function() {
+                return !this.classList.contains('legend')
+            }).remove();
+
+            //add the X Axis
+            this.svg.append("g")
+                .attr("transform", "translate(0," + this.height + ")")
+                .attr("class", "axis")
+                .call(d3.axisBottom(this.x)
+                    .ticks(10)
+                    .tickFormat(format));
+
+            // add the Y Axis
+            this.svg.append("g")
+                .call(d3.axisLeft(this.y));
+
+        } else {
+            data = this.data;
+        }
+        BIOMES.forEach(function(d, i) {
+            var biome = BIOMES[i];
+            this.plotBiomeWater(data, biome, i);
+        }, this);
+    }
+
+    plotActiveRegions (start, moving) {
+        var area_start = this.data[start].start;
+        var area_stop = this.data[start].stop;
+
+        var ixs = d3.range(area_start, area_stop).map(function (d) {
+            var dte = new Date(2014, 4, 1);
+            var u = +dte;
+            var seconds = parseInt(d);
+            var newU = u + seconds * 1000;
+            var newD = new Date(newU);
+            return newD
+        });
+
+        if (moving == true) {
+            this.plotActiveRegion(ixs, true, area_start);
+        } else {
+            if (this.data[start]['important'] == true) {
+                this.plotActiveRegion(ixs, false, area_start);
+            }
+        }
+    }
+
+    plotFocus (start) {
+        var area_start = start > 30 ? start - 30 : 0;
+            area_start = area_start + 100 < this.data.length ? area_start : this.data.length - 100;
+        var area_stop = area_start+100;
+
+        var ixs = d3.range(area_start, area_stop).map(function (d) {
+            var dte = new Date(2014, 4, 1);
+            var u = +dte;
+            var seconds = parseInt(d);
+            var newU = u + seconds * 1000;
+            var newD = new Date(newU);
+            return newD
+        });
+
+        this.plotActiveRegion(ixs, true, area_start, '_view_window');
+    }
+
+    plotActiveRegion (x_indices, moving, id, class_='') {
+
+        var area_tracker = this.svg.select('#area_' + parseInt(id))._groups[0][0]
+        var x = this.x
+
+        if (area_tracker == null) {
+
+            if (moving) {
+                this.svg.select('.area'+class_).remove()
+            }
+
+            // define the area
+            var area = d3.area()
+                .x(function (d) {
+                    return x(d);
+                })
+                .y0(this.height)
+                .y1(this.ymin);
+
+            this.svg.append("path")
+                .data([x_indices])
+                .attr("class", "area"+class_)
+                .attr("id", "area_" + parseInt(id))
+                .attr("transform", null)
+                .attr("d", area);
+
+        }
+    }
+
+    plotLegend (biomes) {
+
+        var legendRectHeight = 10;
+        var legendRectWidth = 60;
+        var legendSpacing = 10;
+        var colors = this.colors;
+
+        this.svg.selectAll('.legend').remove();
+
+        biomes.forEach(function(d, i) {
+
+            this.svg.append("text")
+                    .attr("class", "legend")
+                    .attr("id", "legend-text-" + i)
+                    .attr("x", 0 + 2*legendRectWidth)
+                    .attr("y", ((legendSpacing * i) * 2) - (margin.top / 2))
+                    .style("fill", function () {
+                        return colors(d)
+                    })
+                    .style("font-family", "sans-serif")
+                    .style("font-size", "10px")
+                    .text(d);
+
+            // adds line in the same color as used in the graph
+            var thisItem = d3.select("#legend-text-" + i).node();
+            var bb = thisItem.getBBox();
+            var bx = bb.x - bb.width - legendRectWidth;
+            this.svg.append("path")
+                    .attr("class", "legend")
+                    .attr("data-legend-key", i)
+                    .attr("data-color", function () {
+                        return colors(d)
+                    })
+                    .attr("d", "M" + (bb.x - legendSpacing - legendRectWidth) + "," + (bb.y + bb.height / 2) + " L" + (bb.x - legendSpacing) + "," + (bb.y + bb.height / 2))
+                    .style("stroke", function () {
+                        return colors(d)
+                    })
+                    .style("stroke-width", "2px")
+                    .style("fill", "none")
+                    .attr("class", this.lineStyle(d))
+                    .attr("height", legendRectHeight)
+                    .attr("width", legendRectWidth)
+        }, this);
+    }
+
+    plotTimeTracker (seconds) {
+
+        // we update the position of this line
+        var time_tracker = this.svg.select('.time_tracker')._groups[0][0]
+
+        var dte = new Date(2014, 4, 1);
+        var u = +dte;
+        var newU = u + seconds*1000;
+        var newD = new Date(newU);
+
+        var x = this.x
+
+        if (time_tracker == null) {
+
+            this.svg.append("line")
+                .attr("x1", x(newD))  //<<== change your code here
+                .attr("y1", this.height)
+                .attr("x2", x(newD))  //<<== and here
+                .attr("y2", this.ymin)
+                .attr('class', 'time_tracker')
+                .attr("transform", null)
+                .style("stroke-width", 3)
+                .style("stroke", "black")
+                .style("fill", "none");
+
+        } else {
+            // move the line
+            d3.select(time_tracker)
+                .attr("transform", "translate(" + x(newD) + ",0)")
+                .transition()
+        }
+    }
+
+    plotInterval (start, stop) {
+        alert('Hello World! -' + _args[0]);
+    }
+
+};
